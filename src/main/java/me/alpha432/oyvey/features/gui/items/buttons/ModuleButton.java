de580deaebb764b160dfdc5486d8b41aa6c5a797
Ry-ll <andresquintanaort@gmail.com>
2026-01-19 19:30:32 -0600

    Refactor ModuleButton class and update imports

--- src/main/java/me/alpha432/oyvey/features/gui/items/buttons/ModuleButton.java
+++ src/main/java/me/alpha432/oyvey/features/gui/items/buttons/ModuleButton.java
@@ -1,150 +1,48 @@
-package me.alpha432.oyvey.features.gui.items.buttons;
+package me.oevy.gui.components;
 
-import me.alpha432.oyvey.features.gui.items.Item;
-import me.alpha432.oyvey.features.modules.Module;
-import me.alpha432.oyvey.features.settings.Bind;
-import me.alpha432.oyvey.features.settings.Setting;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.resources.sounds.SimpleSoundInstance;
-import net.minecraft.sounds.SoundEvents;
+import me.oevy.features.modules.Module;
+import me.oevy.gui.theme.PurpleTheme;
+import me.oevy.util.FontUtil;
+import me.oevy.util.RenderUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+public class ModuleButton extends Button {
 
-public class ModuleButton
-        extends Button {
     private final Module module;
-    private List<Item> items = new ArrayList<>();
-    private boolean subOpen;
 
     public ModuleButton(Module module) {
-        super(module.getName());
         this.module = module;
-        this.initSettings();
-    }
-
-    public void initSettings() {
-        ArrayList<Item> newItems = new ArrayList<>();
-        if (!this.module.getSettings().isEmpty()) {
-            for (Setting<?> setting : this.module.getSettings()) {
-                if (setting.getValue() instanceof Boolean && !setting.getName().equals("Enabled")) {
-                    newItems.add(new BooleanButton((Setting<Boolean>) setting));
-                }
-                if (setting.getValue() instanceof Bind && !setting.getName().equalsIgnoreCase("Keybind") && !this.module.getName().equalsIgnoreCase("Hud")) {
-                    newItems.add(new BindButton((Setting<Bind>) setting));
-                }
-                if ((setting.getValue() instanceof String || setting.getValue() instanceof Character) && !setting.getName().equalsIgnoreCase("displayName")) {
-                    newItems.add(new StringButton((Setting<String>) setting));
-                }
-                if (setting.isColorSetting()) {
-                    newItems.add(new ColorButton((Setting<java.awt.Color>) setting));
-                    continue;
-                }
-                if (setting.isNumberSetting() && setting.hasRestriction()) {
-                    newItems.add(new Slider((Setting<Number>) setting));
-                    continue;
-                }
-                if (!setting.isEnumSetting()) continue;
-                newItems.add(new EnumButton((Setting<Enum<?>>) setting));
-            }
-        }
-        newItems.add(new BindButton((Setting<Bind>) this.module.getSettingByName("Keybind")));
-        this.items = newItems;
+        this.height = 14;
     }
 
     @Override
-    public void drawScreen(GuiGraphics context, int mouseX, int mouseY, float partialTicks) {
-        super.drawScreen(context, mouseX, mouseY, partialTicks);
-        if (!this.items.isEmpty()) {
-            if (this.subOpen) {
-                float height = 16.0f;
-                for (Item item : this.items) {
-                    if (!item.isHidden()) {
-                        item.setLocation(this.x + 1.0f, this.y + height);
-                        item.setWidth(this.width - 9);
-                        item.drawScreen(context, mouseX, mouseY, partialTicks);
-                        height += item.getHeight() + 1f;
-                    }
-                    item.update();
-                }
-            }
+    public void draw(int mouseX, int mouseY) {
+        super.draw(mouseX, mouseY);
+
+        RenderUtil.drawRoundedRect(
+                x, y, width, height, 6,
+                module.isEnabled()
+                        ? PurpleTheme.PRIMARY.getRGB()
+                        : PurpleTheme.PANEL.getRGB()
+        );
+
+        FontUtil.drawString(
+                module.getName(),
+                x + 6,
+                y + 4,
+                module.isEnabled()
+                        ? PurpleTheme.TEXT_ACTIVE.getRGB()
+                        : PurpleTheme.TEXT.getRGB()
+        );
+    }
+
+    @Override
+    public void mouseClicked(int mouseX, int mouseY, int button) {
+        if (hovered && button == 0) {
+            module.toggle();
         }
     }
+}
 
-    @Override
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-        if (!this.items.isEmpty()) {
-            if (mouseButton == 1 && this.isHovering(mouseX, mouseY)) {
-                this.subOpen = !this.subOpen;
-                mc.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1f));
-            }
-            if (this.subOpen) {
-                for (Item item : this.items) {
-                    if (item.isHidden()) continue;
-                    item.mouseClicked(mouseX, mouseY, mouseButton);
-                }
-            }
-        }
-    }
-
-    @Override
-    public void mouseReleased(int mouseX, int mouseY, int releaseButton) {
-        super.mouseReleased(mouseX, mouseY, releaseButton);
-        if (!this.items.isEmpty() && this.subOpen) {
-            for (Item item : this.items) {
-                if (item.isHidden()) continue;
-                item.mouseReleased(mouseX, mouseY, releaseButton);
-            }
-        }
-    }
-
-    @Override
-    public void onKeyTyped(String typedChar, int keyCode) {
-        super.onKeyTyped(typedChar, keyCode);
-        if (!this.items.isEmpty() && this.subOpen) {
-            for (Item item : this.items) {
-                if (item.isHidden()) continue;
-                item.onKeyTyped(typedChar, keyCode);
-            }
-        }
-    }
-
-    @Override
-    public void onKeyPressed(int key) {
-        super.onKeyPressed(key);
-        if (!this.items.isEmpty() && this.subOpen) {
-            for (Item item : this.items) {
-                if (item.isHidden()) continue;
-                item.onKeyPressed(key);
-            }
-        }
-    }
-
-    @Override
-    public int getHeight() {
-        if (this.subOpen) {
-            int height = 16;
-            for (Item item : this.items) {
-                if (item.isHidden()) continue;
-                height += item.getHeight() + 1;
-            }
-            return height;
-        }
-        return 14;
-    }
-
-    public Module getModule() {
-        return this.module;
-    }
-
-    @Override
-    public void toggle() {
-        this.module.toggle();
-    }
-
-    @Override
-    public boolean getState() {
         return this.module.isEnabled();
     }
 }
