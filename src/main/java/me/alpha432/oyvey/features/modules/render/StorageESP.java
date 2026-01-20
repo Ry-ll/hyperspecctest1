e2831914bcc331dc3de4844d857f20d66521f07c
Ry-ll <andresquintanaort@gmail.com>
2026-01-19 20:03:24 -0600

    Refactor StorageESP to use BlockEntity instead of TileEntity

--- src/main/java/me/alpha432/oyvey/features/modules/render/StorageESP.java
+++ src/main/java/me/alpha432/oyvey/features/modules/render/StorageESP.java
@@ -4,14 +4,15 @@ import me.hyperspecctest1.features.modules.Module;
 import me.hyperspecctest1.event.events.Render3DEvent;
 import me.hyperspecctest1.util.RenderUtil;
 import me.hyperspecctest1.features.setting.Setting;
-import net.minecraft.tileentity.*;
+import net.minecraft.world.level.block.entity.*;
 import java.awt.Color;
 
 public class StorageESP extends Module {
 
-    private final Setting<Boolean> chests = register(new Setting("Chests", true));
-    private final Setting<Boolean> shulkers = register(new Setting("Shulkers", true));
-    private final Setting<Boolean> enderchests = register(new Setting("EnderChests", true));
+    private final Setting<Boolean> chests = register(new Setting<>("Chests", true));
+    private final Setting<Boolean> shulkers = register(new Setting<>("Shulkers", true));
+    private final Setting<Boolean> enderchests = register(new Setting<>("EnderChests", true));
+    private final Setting<Boolean> barrels = register(new Setting<>("Barrels", true));
     
     public StorageESP() {
         super("StorageESP", "Highlights storage blocks", Category.VISUAL, true, false, false);
@@ -19,17 +20,21 @@ public class StorageESP extends Module {
 
     @Override
     public void onRender3D(Render3DEvent event) {
-        if (mc.world == null || mc.player == null) return;
+        if (mc.level == null || mc.player == null) return;
 
-        for (TileEntity tile : mc.world.loadedTileEntityList) {
-            if (tile instanceof TileEntityChest && chests.getValue()) {
-                RenderUtil.drawBlockBox(tile.getPos(), new Color(255, 160, 0, 150), true, true);
+        for (BlockEntity blockEntity : mc.level.blockEntityTickers) {
+            
+            if (blockEntity instanceof ChestBlockEntity && chests.getValue()) {
+                RenderUtil.drawBlockBox(blockEntity.getBlockPos(), new Color(255, 160, 0, 150), true, true);
             } 
-            else if (tile instanceof TileEntityShulkerBox && shulkers.getValue()) {
-                RenderUtil.drawBlockBox(tile.getPos(), new Color(255, 0, 100, 150), true, true);
+            else if (blockEntity instanceof ShulkerBoxBlockEntity && shulkers.getValue()) {
+                RenderUtil.drawBlockBox(blockEntity.getBlockPos(), new Color(255, 0, 100, 150), true, true);
             }
-            else if (tile instanceof TileEntityEnderChest && enderchests.getValue()) {
-                RenderUtil.drawBlockBox(tile.getPos(), new Color(120, 0, 255, 150), true, true);
+            else if (blockEntity instanceof EnderChestBlockEntity && enderchests.getValue()) {
+                RenderUtil.drawBlockBox(blockEntity.getBlockPos(), new Color(120, 0, 255, 150), true, true);
+            }
+            else if (blockEntity instanceof BarrelBlockEntity && barrels.getValue()) {
+                RenderUtil.drawBlockBox(blockEntity.getBlockPos(), new Color(150, 75, 0, 150), true, true);
             }
         }
     }
