b03c3cd471689e59704975109fd1e5ae8268e00f
Ry-ll <andresquintanaort@gmail.com>
2026-01-19 19:31:06 -0600

    Refactor EnumButton class and update methods

--- src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EnumButton.java
+++ src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EnumButton.java
@@ -1,52 +1,39 @@
-package me.alpha432.oyvey.features.gui.items.buttons;
+package me.oevy.gui.components.settings;
 
+import me.oevy.features.settings.Setting;
+import me.oevy.gui.components.Button;
+import me.oevy.gui.theme.PurpleTheme;
+import me.oevy.util.FontUtil;
+import me.oevy.util.RenderUtil;
 
-import me.alpha432.oyvey.OyVey;
-import me.alpha432.oyvey.features.gui.OyVeyGui;
-import me.alpha432.oyvey.features.modules.client.ClickGui;
-import me.alpha432.oyvey.features.settings.Setting;
-import me.alpha432.oyvey.util.render.RenderUtil;
-import net.minecraft.ChatFormatting;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.resources.sounds.SimpleSoundInstance;
-import net.minecraft.sounds.SoundEvents;
+public class EnumButton extends Button {
 
-public class EnumButton
-        extends Button {
-    public Setting<Enum<?>> setting;
+    private final Setting<Enum<?>> setting;
 
     public EnumButton(Setting<Enum<?>> setting) {
-        super(setting.getName());
         this.setting = setting;
-        this.width = 15;
+        this.height = 12;
     }
 
     @Override
-    public void drawScreen(GuiGraphics context, int mouseX, int mouseY, float partialTicks) {
-        RenderUtil.rect(context, this.x, this.y, this.x + (float) this.width + 7.4f, this.y + (float) this.height - 0.5f, this.getState() ? (!this.isHovering(mouseX, mouseY) ? OyVey.colorManager.getColorWithAlpha(y, ClickGui.getInstance().color.getValue().getAlpha()) : OyVey.colorManager.getColorWithAlpha(y, ClickGui.getInstance().topColor.getValue().getAlpha())) : (!this.isHovering(mouseX, mouseY) ? 0x11555555 : -2007673515));
-        drawString(this.setting.getName() + " " + ChatFormatting.GRAY + (this.setting.currentEnumName().equalsIgnoreCase("ABC") ? "ABC" : this.setting.currentEnumName()), this.x + 2.3f, this.y - 1.7f - (float) OyVeyGui.getClickGui().getTextOffset(), this.getState() ? -1 : -5592406);
+    public void draw(int mouseX, int mouseY) {
+        RenderUtil.drawRoundedRect(
+                x, y, width, height, 4,
+                PurpleTheme.PRIMARY.getRGB()
+        );
+
+        FontUtil.drawString(
+                setting.getName() + ": " + setting.getValue().name(),
+                x + 4,
+                y + 3,
+                PurpleTheme.TEXT_ACTIVE.getRGB()
+        );
     }
 
     @Override
-    public void update() {
-        this.setHidden(!this.setting.isVisible());
-    }
-
-    @Override
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-        if (this.isHovering(mouseX, mouseY)) {
-            mc.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1f));
+    public void mouseClicked(int mouseX, int mouseY, int button) {
+        if (hovered && button == 0) {
+            setting.nextEnum();
         }
-    }
-
-    @Override
-    public void toggle() {
-        this.setting.increaseEnum();
-    }
-
-    @Override
-    public boolean getState() {
-        return true;
     }
 }
